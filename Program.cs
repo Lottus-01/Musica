f4021face58cf9c640af9455e7d15febf4ad6da1
Vittoria Lopes <Lopes.tt06@gmail.com>
2025-03-11 20:31:52 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine(@"You can turn off the light
+You overshadow even the shining sun, my star
+In your sad gaze, a perfect subject that has the world
+My savior, oh wretched soul
+I don't believe, you're a liar
+When my darkness overlaps with your darkness
+Just leave it to me");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Thread.Sleep(2000);
+Console.WriteLine();
+Console.WriteLine(@"Ruler of my heart
+Ruler of my heart
+Ruler of my heart
+Forever beautiful
+Ruler of my heart
+Ruler of my heart
+Ruler of my heart");
+Console.ResetColor();
+Thread.Sleep(2000);
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine(@"An endless wall
+A fall without day
+In a moment, it all crumbles
+I'm blinded and can't hear
+My world is shattered
+My savior
+Oh wretched woman
+Make me your god
+I can give you everything
+When my darkness overlaps with your darkness
+Just leave it to me");
+Console.ResetColor();
